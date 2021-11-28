451c1be2243a0614f50b03f999db9094845dc067
光安雅義 <lightcheap358@gmail.com>
2021-11-28 10:42:29 +0900

    DataProviderを使ってみた

--- web/tests/Unit/CalculatePointServiceTest.php
+++ web/tests/Unit/CalculatePointServiceTest.php
@@ -9,6 +9,17 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CalculatePointServiceTest extends TestCase
 {
+    public function dataProvider_for_calcPoint(): array
+    {
+        return [
+            '購入金額が0なら0ポイント'      => [0, 0],
+            '購入金額が999なら0ポイント'    => [0, 999],
+            '購入金額が1000なら10ポイント'  => [10, 1000],
+            '購入金額が9999なら99ポイント'  => [99, 9999],
+            '購入金額が10000なら200ポイント'=> [200, 10000],
+        ];
+    }
+
     /**
      * A basic unit test example.
      * @test
@@ -36,5 +47,15 @@ class CalculatePointServiceTest extends TestCase
     {
         $result = CalculatePointService::calcPoint(1000);
         $this->assertSame(10, $result);
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProvider_for_calcPoint
+     */
+    public function calcPoint_データプロバイダを使ったテスト(int $expected, int $amount)
+    {
+        $result = CalculatePointService::calcPoint($amount);
+        $this->assertSame($expected, $result);
     }
 }
